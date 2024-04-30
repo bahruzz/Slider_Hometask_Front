f5a75b3724a72d8fe453253c4c0e9e3b509ef0df
unknown <bahruzea@code.edu.az>
2024-05-01 00:22:31 +0400

    Task

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let slider=document.querySelector(".slider")
+let cons=document.querySelectorAll(".slider .content");
+let lefticon=document.querySelector(".slider .left-icon")
+let righticon=document.querySelector(".slider .right-icon");
+
+
+
